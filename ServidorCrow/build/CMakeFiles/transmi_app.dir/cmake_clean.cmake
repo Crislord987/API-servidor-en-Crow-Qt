94035dac6a7f3f8188b548ba329fa96ef188bc94
Crislord987 <138032833+Crislord987@users.noreply.github.com>
2025-09-28 19:49:49 -0500

    Add files via upload

---
+++ ServidorCrow/build/CMakeFiles/transmi_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transmi_app.dir/src/main.cpp.obj"
+  "CMakeFiles/transmi_app.dir/src/main.cpp.obj.d"
+  "libtransmi_app.dll.a"
+  "transmi_app.exe"
+  "transmi_app.exe.manifest"
+  "transmi_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transmi_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
